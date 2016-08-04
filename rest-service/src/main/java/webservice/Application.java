7060d07af9fa7cd7c15af1c82e7e0b5627d195ce
jimmy.do0704 <jimmy.do0704@gmail.com>
2016-08-04 09:24:17 +0700

    Spring restful webservice first commit

---
+++ rest-service/src/main/java/webservice/Application.java
@@ -0,0 +1,14 @@
+package webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by dvdat on 8/3/16.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ rest-service/src/main/java/webservice/Greeting.java
@@ -0,0 +1,21 @@
+package webservice;
+
+/**
+ * Created by dvdat on 8/3/16.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
